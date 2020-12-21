69e37ac26d1bd1ae46f787f53e52831d13b4d373
Jerry <fenturechance@gmail.com>
2020-12-21 15:30:02 +0800

    first commit

---
+++ docker部屬/variables.js
@@ -0,0 +1,1 @@
+window.myUrl = 'https://www.baidu.com/'
---
+++ 前端專案/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <a :href="myUrl" target="_blank">link</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      myUrl: window.myUrl
+    }
+  }
+}
+</script>
